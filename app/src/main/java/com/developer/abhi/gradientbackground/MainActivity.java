24ef4ca4b0e082f5debdf457d36cd913f76fe754
Abhishek Bhardwaj <abhishekbhardwaj5638@gmail.com>
2017-09-23 20:43:23 -0700

    Added theme into activity_mail.xml and configured the animation in MainActivity

--- app/src/main/java/com/developer/abhi/gradientbackground/MainActivity.java
+++ app/src/main/java/com/developer/abhi/gradientbackground/MainActivity.java
@@ -1,13 +1,48 @@
 package com.developer.abhi.gradientbackground;
 
+import android.graphics.drawable.AnimationDrawable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ConstraintLayout constraintLayout;
+    private AnimationDrawable animationDrawable;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        constraintLayout=(ConstraintLayout)findViewById(R.id.constraintLayout);
+        animationDrawable=(AnimationDrawable)constraintLayout.getBackground();
+
+        animationDrawable.setEnterFadeDuration(5000);
+
+        animationDrawable.setExitFadeDuration(2000);
+
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (animationDrawable != null && !animationDrawable.isRunning()) {
+            // start the animation
+            animationDrawable.start();
+        }
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (animationDrawable != null && animationDrawable.isRunning()) {
+            // stop the animation
+            animationDrawable.stop();
+        }
     }
 }
